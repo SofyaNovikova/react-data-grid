3e05975efcb9a74b56053f61efa0325c2449ebec
Sofya Novikova <sofya.novikava@leverx.com>
2017-07-19 10:21:49 +0300

    fix column comparer

--- packages/react-data-grid/src/ColumnComparer.js
+++ packages/react-data-grid/src/ColumnComparer.js
@@ -9,6 +9,13 @@ function sameColumn(a: Column, b: Column): boolean {
       if ((typeof a[k] === 'function' && typeof b[k] === 'function') || (isValidElement(a[k]) && isValidElement(b[k]))) {
         continue;
       }
+      if (typeof a[k] === 'object' && typeof b[k] === 'object') {
+          for (let k1 in a) {
+            if (!b[k].hasOwnProperty(k1) || a[k][k1] !== b[k][k1] ) {
+                return false;
+            }
+          }
+      }
       if (!b.hasOwnProperty(k) || a[k] !== b[k]) {
         return false;
       }
